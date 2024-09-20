a4a64544962e0682c1d45ac6a9ca24f0c63c3fe1
connortbot <loiconnor8@gmail.com>
2024-09-20 17:07:19 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -18,6 +18,7 @@ User-facing script. Pipeline takes in an input and calls external classes for:
 6. Predict
 
 Start with getting "TinyLlama-1.1B-Chat-v1.0" working!
+https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v1.0
 """
 
 if __name__ == "__main__":
