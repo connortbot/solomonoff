8bb83ae57d1c2a18884c089f102231316ebfd826
connortbot <loiconnor8@gmail.com>
2024-09-20 12:32:28 -0400

    init roadmap

---
+++ pipeline.py
@@ -0,0 +1,22 @@
+"""
+=================================================
+Solomonoff
+Author: Connor Loi, Pranav Bedi, Jonathan Wang
+Version: v0.0.0
+=================================================
+"""
+
+
+"""
+File: pipeline.py
+User-facing script. Pipeline takes in an input and calls external classes for:
+1. Tokenization
+2. Embeddings
+3. Positional Encoding
+4. N x Transformer Block
+5. Output Lyaer ? 
+6. Predict
+"""
+
+if __name__ == "__main__":
+    pass
